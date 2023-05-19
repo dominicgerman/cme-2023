a2ce4262a710a895a175f571b82bece6d302f159
Dominic German <dominicgerman@gmail.com>
2023-05-19 09:50:33 -0500

    Deploy build, ryan preview

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '660fee70638698495537');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '1aa7a5ba3898f14b11a2');
