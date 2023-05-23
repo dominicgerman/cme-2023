e6705662be7f96cc7f0f23f061a0c4906ab965cd
Dominic German <dominicgerman@gmail.com>
2023-05-23 18:43:28 -0500

    Deploy test v2

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '6e0ebdf8886705c24d28');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '3475237ac6954ad274e1');
