96904d4e9d4aa0f0bc3727d724047a27e2a51a0e
Dominic German <dominicgerman@gmail.com>
2023-05-31 15:28:06 -0500

    Prod test

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '21bfde06a3a60dd27e85');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '84150ac6641686027c02');
